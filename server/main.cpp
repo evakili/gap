5063dc9b5b8d53d1c65af5b6a48571eddfcbf824
evakili <evakili@users.noreply.github.com>
2018-08-26 23:12:54 +0430

    make fd private

--- server/main.cpp
+++ server/main.cpp
@@ -17,33 +17,34 @@ void error(const char* message) {
 }
 
 struct client_socket {
-    client_socket(int newfd) : fd { newfd } {
-        if (fd < 0)
+    client_socket(int newfd) : fd_ { newfd } {
+        if (fd_ < 0)
             error("ERROR on accept.");
     }
 
     auto read(char* buffer, int len) {
         bzero(buffer, len);
-        auto n = ::read(fd, buffer, len - 1);
+        auto n = ::read(fd_, buffer, len - 1);
         if (n < 0)
             error("ERROR reading from socket.");
         return n;
     }
 
     auto write(const char* buffer, int len) {
-        auto n = ::write(fd, buffer, len);
+        auto n = ::write(fd_, buffer, len);
         if (n < 0)
             error("ERROR writing to socket.");
         return n;
     }
 
-    int fd;
+private:
+    int fd_;
 };
 
 struct server_socket {
     server_socket() {
-        fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (fd <= 0)
+        fd_ = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd_ <= 0)
             error("ERROR opening socket.");
     }
 
@@ -53,21 +54,22 @@ struct server_socket {
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = INADDR_ANY;
         serv_addr.sin_port = htons(portno);
-        if (bind(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        if (bind(fd_, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
             error("ERROR on binding.");
     }
 
     void listen() {
-        ::listen(fd, 5) ;
+        ::listen(fd_, 5) ;
     }
 
     auto accept_a_client() {
         struct sockaddr_in cli_addr;
         auto clilen = sizeof(cli_addr);
-        return client_socket{ accept(fd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen) };
+        return client_socket{ accept(fd_, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen) };
     }
 
-    int fd;
+private:
+    int fd_;
 };
 
 int main(int argc, char *argv[]) {
