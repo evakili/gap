b5a6abdd530613a6b5bb36101735e29db3fe342c
evakili <evakili@users.noreply.github.com>
2018-08-26 18:16:59 +0430

    server: move variable declarations near usage

--- server/main.cpp
+++ server/main.cpp
@@ -17,22 +17,18 @@ void error(const char* message) {
 }
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd, portno, clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-
     if (argc < 2) {
         fprintf(stderr, "ERROR, no port provided.\n");
         exit(1);
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket.");
 
+    struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
+    auto portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
@@ -40,13 +36,16 @@ int main(int argc, char *argv[]) {
         error("ERROR on binding.");
 
     listen(sockfd, 5) ;
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+
+    struct sockaddr_in cli_addr;
+    auto clilen = sizeof(cli_addr);
+    auto newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
     if (newsockfd < 0)
         error("ERROR on accept.");
     
+    char buffer[256];
     bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
+    auto n = read(newsockfd, buffer, 255);
     if (n < 0)
         error("ERROR reading from socket.");
 
