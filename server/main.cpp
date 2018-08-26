1f010dd86f4c5bd1f3b8713bd93bc77d045e74ea
evakili <evakili@users.noreply.github.com>
2018-08-26 18:52:19 +0430

    wrap socket file descriptor in stream_socket struct

--- server/main.cpp
+++ server/main.cpp
@@ -16,42 +16,48 @@ void error(const char* message) {
     exit(1);
 }
 
+struct stream_socket {
+    int fd;
+};
+
 auto get_a_stream_socket() {
-    auto sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock > 0)
+    stream_socket sock{};
+    sock.fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock.fd > 0)
         return sock;
     error("ERROR opening socket.");
-    return -1;
+    return stream_socket{};
 }
 
-auto bind_stream_socket_to_any(int sockfd, int portno) {
+auto bind_stream_socket_to_any(stream_socket sock, int portno) {
     struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(sock.fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding.");
 }
 
-auto accept_a_client(int sockfd) {
+auto accept_a_client(stream_socket sock) {
     struct sockaddr_in cli_addr;
     auto clilen = sizeof(cli_addr);
-    auto newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
-    if (newsockfd < 0)
+    stream_socket newsock{};
+    newsock.fd = accept(sock.fd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+    if (newsock.fd < 0)
         error("ERROR on accept.");
-    return newsockfd;
+    return newsock;
 }
 
-auto read_from_socket(int sockfd, char* buffer, int len) {
+auto read_from_socket(stream_socket sock, char* buffer, int len) {
     bzero(buffer, len);
-    auto n = read(sockfd, buffer, len - 1);
+    auto n = read(sock.fd, buffer, len - 1);
     if (n < 0)
         error("ERROR reading from socket.");
 }
 
-auto write_to_socket(int sockfd, const char* buffer, int len) {
-    auto n = write(sockfd, buffer, len);
+auto write_to_socket(stream_socket sock, const char* buffer, int len) {
+    auto n = write(sock.fd, buffer, len);
     if (n < 0)
         error("ERROR writing to socket.");
 }
@@ -62,19 +68,19 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    auto sockfd = get_a_stream_socket();
+    auto sock = get_a_stream_socket();
 
-    bind_stream_socket_to_any(sockfd, atoi(argv[1]));
+    bind_stream_socket_to_any(sock, atoi(argv[1]));
 
-    listen(sockfd, 5) ;
+    listen(sock.fd, 5) ;
 
-    auto newsockfd = accept_a_client(sockfd);
+    auto newsock = accept_a_client(sock);
     
     char buffer[256];
-    read_from_socket(newsockfd, buffer, 256);
+    read_from_socket(newsock, buffer, 256);
     printf("Here is the message: %s\n", buffer);
 
-    write_to_socket(newsockfd, "I got your message", 18);
+    write_to_socket(newsock, "I got your message", 18);
 
     return 0;
 }
