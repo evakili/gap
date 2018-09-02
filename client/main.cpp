adda48b616f01407d26707b3082b2cb982d3567f
Esmaeil Vakili <vakili@payampardaz.local>
2018-09-03 04:15:51 +0430

    client: move variable declarations near usage

--- client/main.cpp
+++ client/main.cpp
@@ -18,28 +18,23 @@ void error(const char* message) {
 }
 
 int main(int argc, char *argv[]) {
-    int sockfd, portno, n;
-
-    struct sockaddr_in serv_addr;
-    struct hostent* server;
-
-    char buffer[256];
     if (argc < 3) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
 
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    auto portno = atoi(argv[2]);
+    auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket.");
     
-    server = gethostbyname(argv[1]);
+    auto server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host.\n");
         exit(0);
     }
 
+    struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *) server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
@@ -48,11 +43,13 @@ int main(int argc, char *argv[]) {
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting.");
     printf("Please enter the message: ");
+
+    char buffer[256];
     bzero(buffer, 256);
     fgets(buffer, 255, stdin);
 
     buffer[strlen(buffer) - 1] = '\0'; // remove \n from input
-    n = write(sockfd, buffer, strlen(buffer));
+    auto n = write(sockfd, buffer, strlen(buffer));
     if (n < 0)
         error("ERROR writing to socket.");
     
