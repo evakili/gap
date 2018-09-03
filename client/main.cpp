955eb08241892af77865da136ca2981d765460ff
Esmaeil Vakili <vakili@payampardaz.local>
2018-09-03 05:11:10 +0430

    wrap client functionality in client class

--- client/main.cpp
+++ client/main.cpp
@@ -17,32 +17,6 @@ void error(const char* message) {
     exit(0);
 }
 
-auto get_socket() {
-    auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket.");
-    return sockfd;
-}
-
-auto socket_connect(int sockfd, struct sockaddr_in serv_addr) {
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR connecting.");
-}
-
-auto socket_write(int sockfd, const char* buffer) {
-    auto n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to socket.");
-    return n;
-}
-
-auto socket_read(int sockfd, char* buffer, size_t length) {
-    auto n = read(sockfd, buffer, length - 1);
-    if (n < 0)
-        error("ERROR reading from socket.");
-    return n;
-}
-
 auto get_server_address(const char* name, int portno) {
     auto server = gethostbyname(name);
     if (server == NULL) {
@@ -59,16 +33,45 @@ auto get_server_address(const char* name, int portno) {
     return serv_addr;
 }
 
+struct client {
+    client(const char* address, int port) {
+        fd_ = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd_ < 0)
+            error("ERROR opening socket.");
+        connect(get_server_address(address, port));
+    }
+
+    auto write(const char* buffer) {
+        auto n = ::write(fd_, buffer, strlen(buffer));
+        if (n < 0)
+            error("ERROR writing to socket.");
+        return n;
+    }
+
+    auto read(char* buffer, size_t length) {
+        auto n = ::read(fd_, buffer, length - 1);
+        if (n < 0)
+            error("ERROR reading from socket.");
+        return n;
+    }
+
+private:
+    void connect(struct sockaddr_in serv_addr) {
+        if (::connect(fd_, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+            error("ERROR connecting.");
+    }
+
+    int fd_;
+};
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
 
-    auto sockfd = get_socket();
+    auto clnt = client{ argv[1], atoi(argv[2]) };
     
-    socket_connect(sockfd, get_server_address(argv[1], atoi(argv[2])));
-
     printf("Please enter the message: ");
 
     char buffer[256];
@@ -76,10 +79,10 @@ int main(int argc, char *argv[]) {
     fgets(buffer, 255, stdin);
 
     buffer[strlen(buffer) - 1] = '\0'; // remove \n from input
-    socket_write(sockfd, buffer);
+    clnt.write(buffer);
 
     bzero(buffer, 256);
-    socket_read(sockfd, buffer, 256);
+    clnt.read(buffer, 256);
     printf("%s\n", buffer);
     return 0;
 }
