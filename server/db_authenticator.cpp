fc3035a434e4948a77e7008fdb1c66eeb17f3b81
Esmaeil Vakili <vakili@payampardaz.local>
2018-10-08 04:46:13 +0330

    add user calss with fake authentication

--- server/db_authenticator.cpp
+++ server/db_authenticator.cpp
@@ -1,11 +1,13 @@
 #include "db_authenticator.h"
+#include "db/user.h"
 
 namespace gap {
 namespace server {
 
     bool db_authenticator::authenticate(const credentials& creds)
     {
-        return true;
+        db::user u{ creds.first, creds.second };
+        return u.authenticate(creds.second);
     }
 }
 }
---
+++ server/include/db/user.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+namespace gap {
+namespace server {
+    namespace db {
+        struct user {
+        public:
+            user(std::string name, std::string password)
+                : name_{ name }, password_{ password }
+            {}
+
+            std::string name() const {
+                return name_;
+            }
+
+            std::string password() const {
+                return password_;
+            }
+            
+            void name(std::string name) {
+                name_ = name;
+            }
+
+            void password(std::string password) {
+                password_ = password;
+            }
+
+            bool authenticate(std::string password) {
+                return password_ == password;
+            }
+
+        private:
+            std::string name_;
+            std::string password_;
+        };
+    }
+}
+}
