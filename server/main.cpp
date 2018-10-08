ddbdd95e40955ba45efbe3b3456791a808b0d356
Esmaeil Vakili <vakili@payampardaz.local>
2018-10-08 07:33:13 +0330

    add database creation option

--- server/main.cpp
+++ server/main.cpp
@@ -11,15 +11,31 @@
 #include "server.h"
 #include "db_authenticator.h"
 
+#include <odb/database.hxx>
+#include <odb/schema-catalog.hxx>
+#include <odb/sqlite/database.hxx>
+#include "db/user.h"
+#include "db/user_odb.h"
+
 int main(int argc, char *argv[]) {
-    auto cmd_line = argh::parser{ argv };
+    auto cmd_line = argh::parser{ argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION };
     unsigned short portno{};
     cmd_line({ "-p", "--port" }, 9900) >> portno;
     std::string db_path{};
     cmd_line({ "-d", "--database" }, "gap.db") >> db_path;
+    bool db_create = cmd_line[{ "-c", "--create-database" }];
 
     try {
         boost::asio::io_service io_service;
+
+        if (db_create) {
+            auto db = std::make_unique<odb::sqlite::database>(db_path, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+            odb::transaction t{ db->begin() };
+            odb::schema_catalog::create_schema(*db, "", false);
+            auto admin = gap::server::db::user{ "admin", "admin" };
+            db->persist(admin);
+            t.commit();
+        }
 
         auto auth = gap::server::db_authenticator{ db_path };
 
